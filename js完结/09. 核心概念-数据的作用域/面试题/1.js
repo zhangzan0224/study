95f3fa266a46b81ab14586d182bfe777f0f9f65b
zhangzan0224 <zhangzan0224@126.com>
2023-10-15 23:06:00 +0800

    数据作用域

--- js完结/09. 核心概念-数据的作用域/面试题/1.js
+++ js完结/09. 核心概念-数据的作用域/面试题/1.js
@@ -1,7 +1,8 @@
 // 下面的代码输出什么
 // 下面的代码输出什么
 
-console.log(a, b, c);
+console.log(a, b, c); // undefined, undefined, function: c
 var a = 1;
 var b = function () {};
+
 function c() {}
--- js完结/09. 核心概念-数据的作用域/面试题/2.js
+++ js完结/09. 核心概念-数据的作用域/面试题/2.js
@@ -4,10 +4,11 @@ var a = 1,
   b = 2;
 
 function m1() {
-  console.log(a);
+  console.log(a); // undefined, a是在函数内部定义了，需要提升到顶部
   var a = 3;
+
   function m2() {
-    console.log(a, b);
+    console.log(a, b); // 3 , 2
   }
   m2();
 }
