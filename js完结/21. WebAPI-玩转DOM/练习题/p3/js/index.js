04e0cb3b162be45961f96d5a26711c873b7c66dc
zhangzan0224 <zhangzan0224@126.com>
2023-10-26 21:54:52 +0800

    dom操作

--- js完结/21. WebAPI-玩转DOM/练习题/p3/js/index.js
+++ js完结/21. WebAPI-玩转DOM/练习题/p3/js/index.js
@@ -1,14 +1,33 @@
 /**
  * 根据协议的多选框是否选中设置注册按钮状态
  */
-function setSubmitButtonStatus() {}
+function setSubmitButtonStatus() {
+  var policy = document.querySelector('.policy input')
+  var submitBtn = document.querySelector('button[type="submit"]')
+  submitBtn.disabled = !policy.checked
+}
 
 /**
  * 根据手机号文本框中的文本，设置发送验证码按钮的状态
  */
-function setSendCodeButtonStatus() {}
+function setSendCodeButtonStatus() {
+  // 首先获取手机号文本框中的文本
+  var phone = document.querySelector('.txt').value
+  // 然后获取验证码按钮的元素
+  var codeBtn = document.querySelector('button[type="button"]')
+  // 判断手机号文本框中的文本是否为空或者是否符合手机号码的正则表达式来去判断验证码的按钮是否禁用
+  codeBtn.disabled = !/^[1][3,4,5,7,8][0-9]{9}$/.test(phone)
+}
 
 /**
  * 根据当前选中的爱好，设置已选择爱好文本
  */
-function setSelectedLoves() {}
+function setSelectedLoves() {
+  // 首先获取爱好的多选框选中的元素
+  var loves = document.querySelectorAll('select option:checked')
+  console.log(loves);
+  // 然后获取已选择爱好的文本
+  var lovesText = document.querySelector('.loves')
+  // 判断爱好的多选框选中的元素是否为空，如果为空则不显示已选择爱好的文本，如果不为空则显示已选择爱好的文本
+  lovesText.innerText = loves.length ? '已选择的爱好：' + [].slice.call(loves).map(item => item.innerText) : '已选择的爱好：' + ''
+}
