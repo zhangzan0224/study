442d2829cee29982b462e17187135aebb2a1daf1
zhangzan0224 <zhangzan0224@126.com>
2023-10-26 20:39:35 +0800

    小人移动

--- js完结/21. WebAPI-玩转DOM/练习题/p2/js/index.js
+++ js完结/21. WebAPI-玩转DOM/练习题/p2/js/index.js
@@ -1,3 +1,20 @@
 // 每隔一段时间，切换英雄的图片，让英雄动起来
-
+function $(selector) {
+  return document.querySelector(selector);
+}
+var imgIndex = 1;
+setInterval(function () {
+  imgIndex++;
+  if (imgIndex > 4) {
+    imgIndex = 1;
+  }
+  $("#hero").src = './img/' + imgIndex + '.png';
+}, 200);
 // 每隔一段时间，改变英雄的位置，让英雄向右移动
+
+var remove = 0;
+setInterval(function () {
+    remove++;
+    $("#hero").style.left = remove + 'px';
+  },
+  100);
