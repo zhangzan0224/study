3ab815bb33d2e64a6436680425170a1a2cd939aa
zhangzan0224 <zhangzan0224@126.com>
2023-10-31 22:46:24 +0800

    随机颜色

--- js完结/24. WebAPI-DOM尺寸和位置/练习题/p2/js/index.js
+++ js完结/24. WebAPI-DOM尺寸和位置/练习题/p2/js/index.js
@@ -24,21 +24,47 @@ setInterval(() => {
   if (left > maxLeft) {
     left = maxLeft
     disX = -disX;
+    changeBackColor()
   }
 
   if (top > maxTop) {
     top = maxTop
     disY = -disY;
+    changeBackColor()
   }
 
   if (left < 0) {
     left = 0
     disX = -disX;
+    changeBackColor()
   }
   if (top < 0) {
     top = 0
     disY = -disY;
+    changeBackColor()
   }
   ball.style.left = `${left}px`;
   ball.style.top = `${top}px`;
 }, 20); // Change the interval time (in milliseconds) as needed
+
+// 生成指定范围内的随机数
+/**
+ * 生成一个介于最小值和最大值之间的随机数。
+ * @param {number} min - 最小值。
+ * @param {number} max - 最大值。
+ * @returns {number} - 生成的随机数。
+ */
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+/**
+ * 改变球元素的背景颜色为随机的RGB值。
+ *
+ * @param {number} min - RGB组件的最小值。
+ * @param {number} max - RGB组件的最大值。
+ * @return {void} 此函数不返回任何值。
+ */
+function changeBackColor() {
+  ball.style.backgroundColor = 'rgb(' + getRandom(0, 256) + ',' + getRandom(0, 256) + ',' + getRandom(0, 256) + ')'
+}
