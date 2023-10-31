36799daa50d71ba6c03c9c835a4a94bd62abbe67
zhangzan0224 <zhangzan0224@126.com>
2023-10-31 22:35:53 +0800

    小球移动js

--- js完结/24. WebAPI-DOM尺寸和位置/练习题/p2/js/index.js
+++ js完结/24. WebAPI-DOM尺寸和位置/练习题/p2/js/index.js
@@ -1,1 +1,44 @@
 // 让小球向右下运动，遇到边缘后反弹
+
+// 每隔一段时间，让小球向右下运动，改变小球的left 和 top
+const ball = document.querySelector('.ball');
+var disX = 2,
+  disY = 2
+// 让小球向右下运动，遇到边缘后反弹
+var w = document.documentElement.clientWidth
+var h = document.documentElement.clientHeight;
+// 获取小球的宽和高
+var ballW = ball.offsetWidth;
+var ballH = ball.offsetHeight;
+
+// 获取最大的横纵坐标
+var maxLeft = w - ballW
+var maxTop = h - ballH
+setInterval(() => {
+  var reac = ball.getBoundingClientRect();
+  var left = reac.left;
+  var top = reac.top;
+  left += disX;
+  top += disY;
+  // 控制范围
+  if (left > maxLeft) {
+    left = maxLeft
+    disX = -disX;
+  }
+
+  if (top > maxTop) {
+    top = maxTop
+    disY = -disY;
+  }
+
+  if (left < 0) {
+    left = 0
+    disX = -disX;
+  }
+  if (top < 0) {
+    top = 0
+    disY = -disY;
+  }
+  ball.style.left = `${left}px`;
+  ball.style.top = `${top}px`;
+}, 20); // Change the interval time (in milliseconds) as needed
