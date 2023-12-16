989c09e413e0b33ef3b51e62d43c1a3db7fe1521
zhangzan0224 <zhangzan0224@126.com>
2023-12-16 11:01:43 +0800

    模板字符串和函数应用

---
+++ vue3/my-vue-app/src/utils/模板字符串.js
@@ -0,0 +1,16 @@
+ const love1 = '秋葵'
+ const love2 = '菜花'
+ const text = myTag `我喜欢${love1}和${love2}.`
+
+ function myTag(parts) {
+   const args = Array.prototype.slice.call(arguments, 1)
+   //  console.log(`output->`, args)
+   let str = ''
+   for (let i = 0; i < args.length; i++) {
+     str += parts[i] + ':' + args[i]
+   }
+   str += parts[parts.length - 1]
+   return str
+ }
+
+ console.log(`output->`, text)
