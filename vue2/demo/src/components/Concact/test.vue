18dd7d116f3b08f01721ca17949a05dd810d9430
zhangzan0224 <zhangzan0224@126.com>
2024-01-23 23:19:10 +0800

    组件

---
+++ vue2/demo/src/components/Concact/test.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="test-container">
+    <Concact></Concact>
+  </div>
+</template>
+<script>
+import Concact from ".";
+export default {
+  name: "",
+  components: { Concact },
+  data() {
+    return {};
+  },
+  props: {},
+  // 计算属性
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="less" scoped>
+.test-container {
+  width: 400px;
+  height: 400px;
+  border: 2px solid red;
+  background: #000;
+  margin: 0 auto;
+  padding-top: 200px;
+}
+</style>
---
+++ vue2/demo/src/components/ImageLoader/test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="test-container">
+    <ImageLoader  src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?fit=crop&crop=entropy&w=3456&h=2304"
+    placeholder="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?w=100"
+    @load="handleLoaded">
+    </ImageLoader>
+  </div>
+</template>
+<script>
+import ImageLoader from "./";
+export default {
+  name: 'Test',
+  components: {ImageLoader},
+  methods: {
+    handleLoaded () {
+      console.log('tag', '图片加载完成')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.test-container {
+  width: 500px;
+  height: 400px;
+  border: 2px solid;
+  margin: 0 auto;
+}
+</style>
