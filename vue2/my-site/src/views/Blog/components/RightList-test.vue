0a9392e988b362bf4b2f13e42d9c9feae4f227a8
zhangzan <zhangzan0224@126.com>
2024-01-31 16:31:55 +0800

    文章列表组件

---
+++ vue2/my-site/src/views/Blog/components/RightList-test.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>aaaaa <RightList :list="list" @select="handleSelect"></RightList></div>
+</template>
+
+<script>
+import RightList from "./RightList.vue";
+export default {
+  components: {
+    RightList,
+  },
+  data() {
+    return {
+      list: [
+        { name: "a", isSelectd: false },
+        { name: "b", isSelectd: false },
+        {
+          name: "c",
+          isSelectd: true,
+          children: [
+            { name: "c1", isSelectd: false },
+            {
+              name: "c2",
+              isSelectd: false,
+              children: [
+                { name: "c21", isSelectd: false },
+                { name: "c22", isSelectd: false },
+                { name: "c23", isSelectd: false },
+              ],
+            },
+            { name: "c3", isSelectd: false },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSelect(item) {
+      console.log(item);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue2/my-site/src/views/Blog/components/RightList.vue
@@ -0,0 +1,59 @@
+<template>
+  <ul class="right-list-container">
+    <li v-for="(item, index) in list" :key="index">
+      <span @click="handleSelect(item)" :class="{ active: isSelected }">
+        {{ item.name }}
+      </span>
+      <!--嵌套原来的组件-->
+      <RightList
+        v-if="item.children"
+        :list="item.children"
+        @select="handleSelect"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "RightList",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    handleSelect(item) {
+      this.$emit("select", item);
+    },
+  },
+  mounted() {
+    console.log(this.list);
+  },
+};
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+
+.right-list-container {
+  list-style: none;
+  padding: 0;
+
+  .right-list-container {
+    margin-left: 1em;
+  }
+
+  li {
+    min-height: 40px;
+    line-height: 40px;
+    cursor: pointer;
+
+    .active {
+      color: @warn;
+      font-weight: bold;
+    }
+  }
+}
+</style>
