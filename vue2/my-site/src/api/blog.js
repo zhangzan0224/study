7f2b5e5d9817a935b960cf22fc8987d2d5b4b499
zhangzan0224 <zhangzan0224@126.com>
2024-02-01 21:59:15 +0800

    评论接口

--- vue2/my-site/src/api/blog.js
+++ vue2/my-site/src/api/blog.js
@@ -19,3 +19,34 @@ export async function getBlogs(page = 1, limit = 10, categoryid = -1) {
 export async function getBlogTypes() {
 	return await request.get("/api/blogtype");
 }
+
+
+/**
+ * 获取博客详情
+ */
+
+export function getBlogDetail(blogId) {
+	return request.get(`/api/blog/${blogId}`);
+}
+
+/**
+ * 获取博客评论
+ */
+
+export function getBlogComments(blogId, page = 1, limit = 10) {
+	return  request.get('/api/comment', {
+		// 通过params选项设置query参数
+		params: {
+			blogId,
+			page,
+			limit,
+		},
+	});
+}
+
+/**
+ * 提交评论
+ */
+export function postComment(commentInfo) {
+	return request.post("/api/comment",commentInfo);
+}
