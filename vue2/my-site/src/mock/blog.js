d0c5a1edd3f7b85d375d4fc30d6d433e0834c336
zhangzan <zhangzan0224@126.com>
2024-02-01 17:14:38 +0800

    修改mock

--- vue2/my-site/src/mock/blog.js
+++ vue2/my-site/src/mock/blog.js
@@ -1,5 +1,5 @@
-import Mock from 'mockjs'
-import qs from 'querystring'
+import Mock from "mockjs";
+import qs from "querystring";
 // 对文章的类型进行模拟
 
 /**
@@ -11,41 +11,44 @@ import qs from 'querystring'
  * @see {@link https://github.com/nuysoft/Mock} for more information about Mock.js
  */
 Mock.mock("/api/blogtype", "get", {
- code: 0,
- msg: "",
- "data|10-20": [
-  {
-   "id|+1": 1,
-   name: "分类@id",
-   "articleCount|0-300": 0,
-   "order|+1": 1,
-  },
- ],
+  code: 0,
+  msg: "",
+  "data|10-20": [
+    {
+      "id|+1": 1,
+      name: "分类@id",
+      "articleCount|0-300": 0,
+      "order|+1": 1,
+    },
+  ],
 });
 
 Mock.mock(/^\/api\/blog(\?.+)?$/, "get", function (options) {
-	const query = qs.parse(options.url);
-	return Mock.mock({
-		code: 0,
-		msg: "",
-		data: {
-			"total|2000-3000": 0,
-			[`rows|${query.limit | 10}`]: [
-				{
-					"id": "@guid",
-					title: "@ctitle(10,20)",
-					description: "@cparagraph(1,10)",
-					category: {
-						"id|1-10": 0,
-						name: "分类@id",
-					},
-					"scanNumber|0-3000": 0,
-					"commentNumber|0-300": 30,
-					thumb: Mock.Random.image("300x250", "#000", "#fff", "Random Image"),
-					createDate: `@date('T')`,
-				},
-			],
-		},
-	});
-})
-
+  const query = qs.parse(options.url);
+  return Mock.mock({
+    code: 0,
+    msg: "",
+    data: {
+      "total|2000-3000": 0,
+      [`rows|${query.limit | 10}`]: [
+        {
+          id: "@guid",
+          title: "@ctitle(10,20)",
+          description: "@cparagraph(1,10)",
+          category: {
+            "id|1-10": 0,
+            name: "分类@id",
+          },
+          "scanNumber|0-3000": 0,
+          "commentNumber|0-300": 30,
+          // 生成随机图片，在图片和null之间随机
+          "thumb|1": [
+            Mock.Random.image("300x250", "#000", "#fff", "Random Image"),
+            null,
+          ],
+          createDate: `@date('T')`,
+        },
+      ],
+    },
+  });
+});
