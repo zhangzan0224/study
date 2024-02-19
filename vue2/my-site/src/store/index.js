377c70146352f5063d6d6adf2f7f37fffc945e08
zhangzan0224 <zhangzan0224@126.com>
2024-02-19 22:39:04 +0800

    project页面

--- vue2/my-site/src/store/index.js
+++ vue2/my-site/src/store/index.js
@@ -2,11 +2,13 @@ import Vuex from "vuex";
 import Vue from "vue";
 import banner from "./banner";
 import setting from "./setting";
+import project from "@/store/project";
 Vue.use(Vuex);
 const store = new Vuex.Store({
   modules: {
     banner,
     setting,
+    project,
   },
   strict: true,
 });
